3cb5b8c9634073952c620312390f74a0970cd107
mazagri3 <obrida@live.com>
2025-04-19 15:06:55 -0400

    feat: implement base Vehicle class with common functionality

--- Develop/src/classes/Vehicle.ts
+++ Develop/src/classes/Vehicle.ts
@@ -3,20 +3,41 @@ import Driveable from '../interfaces/Driveable.js';
 
 // Vehicle class that implements Driveable interface
 class Vehicle implements Driveable {
-  // Declare properties of the Vehicle class
+  // Common properties for all vehicles
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
   started: boolean;
   currentSpeed: number;
 
   // Constructor for the Vehicle class
-  constructor() {
+  constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number) {
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
     this.started = false;
     this.currentSpeed = 0;
   }
 
   // Method to print vehicle details
   printDetails(): void {
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
     console.log(`Vehicle started: ${this.started}`);
-    console.log(`Vehicle current speed: ${this.currentSpeed} mph`);
+    console.log(`Current speed: ${this.currentSpeed} mph`);
   }
 
   // Method to start the vehicle
